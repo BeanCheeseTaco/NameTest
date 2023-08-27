ed60fb84e60bfda2c4faba7e5d7994bb980971d0
MaryLou <marylou@iastate.edu>
2023-08-26 23:51:35 -0600

    Commiting my change via VS Code

--- YourName.py
+++ YourName.py
@@ -3,4 +3,4 @@ username = input("Enter username:")
 print("Username is: " + username)
 
 myusername = username + ' Potter'
-print("My name is " + myusername)
+print("My name is " + myusername + " Welcome!")
